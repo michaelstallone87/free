2c081212f986960b053345dd92b5896fe631b78e
Michael Stallone <michael.stallone87@gmail.com>
2020-08-21 21:18:52 -0300

    initial file

---
+++ coffee.py
@@ -0,0 +1,11 @@
+while True:
+	coffee = 'coffee situation.'
+	if coffee == 'empty':
+		print('fill')
+	elif coffee == 'full':
+		print('drink')
+		break
+	else:
+		print("it's ok")
+	pass
+print('so proceed...')
